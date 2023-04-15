de3eb34f2643192f2ddf4da92865b26a3d6697f3
Jolan95 <jolan.aubry@hotmail.fr>
2023-04-15 02:10:34 +0200

    devis page

---
+++ src/components/iframe.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Iframe() {
+  return (
+    <div>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d844140.0340429838!2d2.1809595957836203!3d49.138156701558444!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e64482344a1697%3A0x40b82c3688b2d90!2s95270%20Saint-Martin-du-Tertre!5e0!3m2!1sfr!2sfr!4v1681515941596!5m2!1sfr!2sfr" width="600" height="450"  allowFullScreen="" loading="lazy" title="target" referrerPolicy="no-referrer-when-downgrade"></iframe> 
+    </div>
+  )
+}
--- src/routes/devis.js
+++ src/routes/devis.js
@@ -2,19 +2,47 @@ import React from 'react'
 import Header from '../components/header'
 import Footer from '../components/footer'
 import Form  from '../components/form'
+import Iframe from "../components/iframe"
+import IconLocalisation from "../image/icons/localisation.png"
+import IconTelephone from "../image/icons/tel.png"
+import IconMail from "../image/icons/mail.png"
+
 
 export default function Devis() {
   return (
     <>
         <Header active="devis"></Header>
-        <div className='px-5  sm:p-20'>
-            <h1 className='text-2xl font-semibold'>Demande de contact</h1>
-            <div className='wrapper-form'>
-                <Form>
-                </Form>
+        <div className=' p-5  sm:p-10'>
+            <h1 className='text-2xl font-semibold text-center py-0 mg:py-3'>Contact</h1>
+            <div className="lg:grid lg:grid-cols-2 max-w-screen-xl mx-auto mx-7 md:mx-10 xl:mx-auto pb-7 lg:py-7 text-md md:text-lg ">
+                <div className='wrapper-form '>
+                    <h2 className='text-lg font-semibold'>Demande de contact</h2>
+                    <Form>
+                    </Form>
+                </div>
+                <div>
+                    <div className='border-2 border-black p-5 mb-5 mt-7 lg:mt-0'>
+                        <h1 className='text-center text-xl text-cyan-600 font-semibold'>
+                        Road Runners
+                        </h1>
+                        <div>
+                            <div className='flex items-center'>
+                                <img src={IconTelephone} alt="icon" className='h-4 w-auto'></img> : 06.34.23.15.43
+                            </div>
+                            <div className='flex items-center'>
+                                <img src={IconMail} alt="icon" className='h-4 w-auto'></img> : RRUNNERS@GMAIL.COM
+                            </div>
+                            <div className='flex items-center'>
+                                <img src={IconLocalisation} alt="icon" className='h-4 w-auto'></img> : 20 RUE ROGER SALENGRO 95270 SAINT-MARTIN-DU-TERTRE
+                            </div>
+                        </div>
+                    </div>
+                    <div className='hidden lg:block'>
+                        <Iframe></Iframe>
+                    </div>
+                </div>
             </div>
         </div>
-        
         <Footer></Footer>
     </>
   )
