ebbc603da3bcc7f6b79b9f23d55d41e7a8336953
Jolan95 <jolan.aubry@hotmail.fr>
2023-04-15 16:46:14 +0200

    page devis et demenagement

--- src/routes/devis.js
+++ src/routes/devis.js
@@ -15,11 +15,6 @@ export default function Devis() {
         <div className=' p-5  sm:p-10'>
             <h1 className='text-2xl font-semibold text-center py-0 mg:py-3'>Contact</h1>
             <div className="lg:grid lg:grid-cols-2 max-w-screen-xl mx-auto mx-7 md:mx-10 xl:mx-auto pb-7 lg:py-7 text-md md:text-lg ">
-                <div className='wrapper-form '>
-                    <h2 className='text-lg font-semibold'>Demande de contact</h2>
-                    <Form>
-                    </Form>
-                </div>
                 <div>
                     <div className='border-2 border-black p-5 mb-5 mt-7 lg:mt-0'>
                         <h1 className='text-center text-xl text-cyan-600 font-semibold'>
@@ -37,9 +32,17 @@ export default function Devis() {
                             </div>
                         </div>
                     </div>
+                    <div className='p-2 bg-slate-500 rounded text-white my-4'>
+                        Pour une question ou un devis veuillez remplir le formulaire, nous vous répondrons dans les plus brefs délais.
+                    </div>
                     <div className='hidden lg:block'>
                         <Iframe></Iframe>
                     </div>
+                </div>
+                <div className='wrapper-form order-first '>
+                    <h2 className='text-xl font-semibold'>Demande de contact</h2>
+                    <Form>
+                    </Form>
                 </div>
             </div>
         </div>
